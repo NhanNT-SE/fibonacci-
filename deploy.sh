fe4fc530ba3a4760850e2926ca7206f002286ea6
NhanNT-SE <nhannt.se1905@gmail.com>
2021-07-01 09:15:07 +0700

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t nhannt1905/fib-client:latest -t nhannt1905/fib-client:$SHA -f .client/Dockerfile ./client
-docker build -t nhannt1905/fib-server:latest -t nhannt1905/fib-server:$SHA -f .server/Dockerfile ./server
-docker build -t nhannt1905/fib-worker:latest -t nhannt1905/fib-worker:$SHA -f .worker/Dockerfile ./worker
+docker build -t nhannt1905/fib-client:latest -t nhannt1905/fib-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nhannt1905/fib-server:latest -t nhannt1905/fib-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nhannt1905/fib-worker:latest -t nhannt1905/fib-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push nhannt1905/fib-client:latest
 docker push nhannt1905/fib-server:latest
